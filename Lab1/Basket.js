5e0a08e29382b6eef4214b13d9a863621ee0917d
VladMertvichenko <34867346+VladMertvichenko@users.noreply.github.com>
2018-01-02 22:13:22 +0200

    Update Basket.js

--- Lab1/Basket.js
+++ Lab1/Basket.js
@@ -1,34 +1,89 @@
-var prodcuts = [{
-	name : "test",
-	price : 12.9,
-	inventory : 20
-}, {
-	name : "test2",
-	price : 30,
-	inventory : 80
-}];
+var prodcuts = [{ 
+	name : "test", 
+	price : 12.9, 
+	inventory : 20 
+	}, { 
+	name : "test2", 
+	price : 30, 
+	inventory : 80 
+}]; 
 
-class ProductLineItem {
-	constructor(product) {
-		//implement
-	}
+class ProductLineItem { 
+
+constructor(productID) { 
+	this.id = productID; 
+	this.amount = 1; 
+	this.product = prodcuts[productID]; 
+} 
+
+getAmount () { 
+	return this.amount; 
+} 
+setAmount(quantity) { 
+	this.amount = quantity; 
+} 
+getProduct() { 
+	return this.product; 
+} 
+
+
+} 
+
+
+var basket = (function(){ 
+	var a = [] 
+	var getIndexProduct = function (productID) { 
+		var index = null; 
+		a.forEach(function(item, i, array) { 
+		if (item.id == productID) { 
+			index = i; 
+		} 
+		}); 
+		return index; 
+	} 
+return { 
+addProduct : function(productID){ 
+	var indexProduct = getIndexProduct(productID); 
+	if(indexProduct == null) { 
+	if (prodcuts[productID].inventory-1 >= 0) { 
+	a.push(new ProductLineItem(productID)); 
+	prodcuts[productID].inventory--; 
+	} else { 
+	alert("Немає продуктів на складі!"); 
+	} 
+	} else { 
+	alert("Ви вже вибрали цей продукт"); 
+	} 
+
+}, 
+removeProduct : function(productID){ 
+	var indexProduct = getIndexProduct(productID); 
+	if(indexProduct == null) { 
+	alert("Ви ще не вибрали цей продукт!"); 
+	} else { 
+	var product = a[indexProduct]; 
+	product.getProduct().inventory += product.getAmount(); 
+	a = a.slice(0,indexProduct).concat(a.slice(indexProduct+1));
+	} 
+}, 
+updateProductQuantity : function(productID, quantity) { 
+	var product = a[getIndexProduct(productID)]; 
+	if ( product.getProduct().inventory - quantity + product.getAmount() >= 0) { 
+	product.getProduct().inventory += product.getAmount() - quantity; 
+	product.setAmount(quantity); 
+} else { 
+alert("Немає продуктів на складі!"); 
+} 
+}, 
+getTotalPrice : function(){ 
+	var totalPrice = 0; 
+	a.forEach(function(item, i, arr) { 
+	totalPrice += item.getAmount() * item.getProduct().price; 
+	}); 
+	alert(totalPrice); 
+	},
+AllMass : function(){
+	return a.length;
 }
-
-
-var basket = (function(){
-	//implement
-	return {
-		addProduct : function(productID){
-			//implement
-		},
-		removeProduct : function(productID){
-			//implement
-		},
-		updateProductQuantity : function(productID, quantity) {
-			//implement
-		},
-		getTotalPrice : function(){
-			//implement
-		}
-	}
-})();
+	} 
+	})();
